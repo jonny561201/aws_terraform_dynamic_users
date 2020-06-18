944495e07060193f47e91a78eb604fadd6cef4a5
Jon Graf <jongraf1@gmail.com>
2020-06-18 16:56:33 -0500

    created custom policy for users that gives them administrative access for time in AWS

--- deployments/common/iam.tf
+++ deployments/common/iam.tf
@@ -11,24 +11,19 @@ resource "aws_iam_access_key" "lb" {
   depends_on = [aws_iam_user.lb]
 }
 
+data "aws_iam_policy_document" "classroom_user_policy_doc" {
+  statement {
+    sid = "1"
+    actions = ["*"]
+    resources = ["*"]
+  }
+}
+
 resource "aws_iam_user_policy" "classroom_user_policy" {
   name = "classroom_user_policy${count.index}"
   user = "classroom_user_${count.index}"
   count = var.user_count
-  depends_on = [aws_iam_access_key.lb]
+  depends_on = [aws_iam_access_key.lb, data.aws_iam_policy_document.classroom_user_policy_doc]
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:Describe*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+  policy =data.aws_iam_policy_document.classroom_user_policy_doc.json
 }
